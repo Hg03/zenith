733204fd505b626f070ffa755b327422aaa5dd2a
harish gehlot <gehloth03@gmail.com>
2026-03-15 10:08:00 +0000

    inference pipeline

--- src/zenith/app/inference_endpoint.py
+++ src/zenith/app/inference_endpoint.py
@@ -1,8 +1,57 @@
-from fastapi import FastAPI
+from zenith.pipelines.inference_pipeline import InferencePipeline
+from pydantic import BaseModel, Field, field_validator
+from fastapi import FastAPI, status
+import uvicorn
 
 app = FastAPI()
+
+
+class PredictionInput(BaseModel):
+    person_name: str = Field(..., min_length=1, max_length=50)
+    age: int = Field(..., ge=10, le=100)
+    daily_screen_time_min: int = Field(..., ge=0, le=1440)
+    platform: str
+    social_media_time_min: int = Field(..., ge=0, le=1440)
+    negative_interactions_count: int = Field(..., ge=0)
+    positive_interactions_count: int = Field(..., ge=0)
+    sleep_hours: int = Field(..., ge=0, le=24)
+    physical_activity_min: int = Field(..., ge=0, le=1440)
+    anxiety_level: int = Field(..., ge=0, le=10)
+    gender: str
+
+    @field_validator("gender")
+    @classmethod
+    def validate_gender(cls, v):
+        allowed = {"Male", "Female", "Other"}
+        if v not in allowed:
+            raise ValueError(f"gender must be one of {allowed}")
+        return v
+
+    @field_validator("platform")
+    @classmethod
+    def validate_platform(cls, v):
+        allowed = {"Facebook", "Instagram", "Snapchat", "TikTok"}
+        if v not in allowed:
+            raise ValueError(f"platform must be one of {allowed}")
+        return v
+
+
+class ResponseModel(BaseModel):
+    mental_state: str
 
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy", "project": "zenith"}
+
+
+@app.post("/predict", response_model=ResponseModel, status_code=status.HTTP_200_OK)
+def prediction_service(input: PredictionInput):
+    infer_runner = InferencePipeline()
+    features = input.model_dump()
+    result = infer_runner.execute(PredictionInput=features)
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port="8000")
--- src/zenith/pipelines/inference_pipeline.py
+++ src/zenith/pipelines/inference_pipeline.py
@@ -1,6 +1,20 @@
-class InferencePipeline:
-    def __init__(self, config=None):
-        pass
+from zenith.scripts.utils import load_assets
+from zenith.scripts import process, model
 
-    def execute(self):
+
+class InferencePipeline:
+    def __init__(self):
+        model_dir = "artifacts/models/model.skops"
+        preprocessor_dir = "artifacts/preprocessor/preprocessor.skops"
+        self.model = load_assets(model_dir, "model")
+        self.preprocessor = load_assets(preprocessor_dir, "model")
+
+    def execute(self, PredictionInput: dict):
         print("Inference Pipeline Started >")
+        processed_PredictionInput, full_prediction_sample = process.prediction_input(
+            PredictionInput
+        )
+        predictions = model.predict_with_loaded_model(
+            processed_PredictionInput, self.preprocessor, self.model
+        )
+        return predictions
